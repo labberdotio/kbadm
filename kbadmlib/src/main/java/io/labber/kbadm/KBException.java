48034f70d3cfb262de70e0a0008e3a29884fafbd
John Grundback <jgrundback@gmail.com>
2026-01-20 08:34:46 -0800

    Add

---
+++ kbadmlib/src/main/java/io/labber/kbadm/KBException.java
@@ -0,0 +1,15 @@
+package io.labber.kbadm;
+
+public class KBException extends Exception {
+
+	private static final long serialVersionUID = -3468674727337642257L;
+
+	/**
+	 * 
+	 * @param e
+	 */
+	public KBException(Exception e) {
+		super(e);
+	}
+
+}
