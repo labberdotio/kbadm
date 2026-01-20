2f4875e5228e3f0853dc1214c968fd421654a3d6
John Grundback <jgrundback@gmail.com>
2026-01-20 10:39:49 -0800

    PostgreSQL PGVEctor schema stuff

--- kbadmlib/src/main/java/io/labber/kbadm/impl/pgvector/PgVectorStoreDriverImpl.java
+++ kbadmlib/src/main/java/io/labber/kbadm/impl/pgvector/PgVectorStoreDriverImpl.java
@@ -45,6 +45,27 @@ public class PgVectorStoreDriverImpl extends Driver {
 		this.metadata = store.getMetadata();
 	}
 
+	public void init() throws KBException {
+
+		/*
+		 * Main table
+		 */
+		try(
+			Connection conn = this.jdbcTemplate.getDataSource().getConnection();
+			Statement stmt = conn.createStatement();
+		) {
+			stmt.executeUpdate(
+					"CREATE TABLE IF NOT EXISTS " + this.getStoreTableName() + " ("
+							+ " id SERIAL PRIMARY KEY,"
+							+ " name VARCHAR(255) NOT NULL"
+							+ ");"
+			);
+		} catch( SQLException e ) {
+			e.printStackTrace();
+		}
+
+	}
+
 	/**
 	 * 
 	 * @throws KBException
