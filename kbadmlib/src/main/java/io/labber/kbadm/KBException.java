c2797472921f23ba0a63e4222816c8e6320a0f85
John Grundback <jgrundback@gmail.com>
2026-01-25 08:35:15 -0800

    Adding a KBException exception class.
    Adding a KBException exception class.

---
+++ kbadmlib/src/main/java/io/labber/kbadm/KBException.java
@@ -0,0 +1,26 @@
+
+// 
+// Copyright (c) 2026, John Grundback
+// All rights reserved.
+// 
+
+package io.labber.kbadm;
+
+/**
+ * 
+ * @author john
+ *
+ */
+public class KBException extends Exception {
+
+	private static final long serialVersionUID = -3468674727337642257L;
+
+	/**
+	 * 
+	 * @param e
+	 */
+	public KBException(Exception e) {
+		super(e);
+	}
+
+}
