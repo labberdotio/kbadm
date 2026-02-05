1626fbf5ce3f28aca5e18dfb7277476eddd1f882
John Grundback <jgrundback@gmail.com>
2026-02-05 10:24:23 -0800

    Document Repair
    john@controller2.localdomain ~/Documents/development/kbadm>ls -al kbadmlib/src/main/java/io/labber/kbadm/model/Status.java
    -rw-r--r--@ 1 john  staff  574 jan. 25 10:28 kbadmlib/src/main/java/io/labber/kbadm/model/Status.java

--- kbadmlib/src/main/java/io/labber/kbadm/model/Status.java
+++ kbadmlib/src/main/java/io/labber/kbadm/model/Status.java
@@ -2,25 +2,34 @@ package io.labber.kbadm.model;
 
 public enum Status {
 
-	INITIAL("Initial"), 
-	COMPLETE("Complete"), 
-	INCOMPLETE("Incomplete"),
-	FAILED("Failed");
+	INITIAL("Initial", false), 
+	COMPLETE("Complete", false), 
+	INCOMPLETE("Incomplete", false),
+	INCONSISTENT("Inconsistent", true),
+	FAILED("Failed", false);
 
 	private final String status;
+	private final boolean repairable;
 
 	/**
 	 * 
 	 * @param status
+	 * @param repairable
 	 */
 	private Status(
-		String status
+		String status, 
+		boolean repairable
 	) {
 		this.status = status;
+		this.repairable = repairable;
 	}
 
 	public String getStatus() {
 		return this.status;
+	}
+
+	public boolean isRepairable() {
+		return this.repairable;
 	}
 
 }
