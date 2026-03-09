ddcb5ac45961589a2e9386a76e852a39203ffa8d
John Grundback <jgrundback@gmail.com>
2026-03-09 16:49:42 -0700

    App
    john@Johns-MacBook-Pro-13-M2.local ~/Documents/development/kbadm>ls -al kbadmapp/src/main/java/io/labber/kbadm/Application.java
    -rw-r--r--@ 1 john  staff  1412 feb.  7 09:55 kbadmapp/src/main/java/io/labber/kbadm/Application.java

---
+++ kbadmapp/src/main/java/io/labber/kbadm/Application.java
@@ -0,0 +1,39 @@
+
+//
+// Copyright (c) 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, John Grundback
+// All rights reserved.
+//
+
+package io.labber.kbadm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+// import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+// @Configuration
+// @EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {
+})
+@ComponentScan
+@ServletComponentScan
+// public class Application implements CommandLineRunner {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
