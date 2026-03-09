bf9ddff2034599d7731761d8aa3329d030af3b27
John Grundback <jgrundback@gmail.com>
2026-03-09 16:47:37 -0700

    Admin,
    john@Johns-MacBook-Pro-13-M2.local ~/Documents/development/kbadm>ls -al kbadmlib/src/main/java/io/labber/kbadm/store/IStoreAdmin.java
    -rw-r--r--@ 1 john  staff  169 jan. 19 11:51 kbadmlib/src/main/java/io/labber/kbadm/store/IStoreAdmin.java
    john@Johns-MacBook-Pro-13-M2.local ~/Documents/development/kbadm>ls -a kbadmlib/src/main/java/io/labber/kbadm/store/StoreAdmin.java
    kbadmlib/src/main/java/io/labber/kbadm/store/StoreAdmin.java

---
+++ kbadmlib/src/main/java/io/labber/kbadm/store/StoreAdmin.java
@@ -0,0 +1,16 @@
+
+// 
+// Copyright (c) 2026, John Grundback
+// All rights reserved.
+// 
+
+package io.labber.kbadm.store;
+
+/**
+ * 
+ * @author john
+ *
+ */
+public class StoreAdmin implements IStoreAdmin {
+
+}
