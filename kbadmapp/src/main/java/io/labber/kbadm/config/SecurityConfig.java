c492e72ccd1e5a3d544fa4b5bc7ffa9b3fe3818d
John Grundback <jgrundback@gmail.com>
2026-03-09 20:02:39 -0700

    KB App

--- kbadmapp/src/main/java/io/labber/kbadm/config/SecurityConfig.java
+++ kbadmapp/src/main/java/io/labber/kbadm/config/SecurityConfig.java
@@ -1,35 +1,35 @@
-
 //
-// Copyright (c) 2019, 2023, 2024, 2025, John Grundback
-// All rights reserved.
+////
+//// Copyright (c) 2019, 2023, 2024, 2025, John Grundback
+//// All rights reserved.
+////
 //
-
-package io.labber.kbadm.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-// import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-	@Bean
-	// @Order(2)
-	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.csrf(csrf -> csrf.disable()) // Disable CSRF for stateless APIs
-			.authorizeHttpRequests(
-				authorizeRequests -> authorizeRequests
-					.anyRequest().permitAll())
-					.sessionManagement(sess -> sess
-					.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // No sessions
-				);
-		return http.build();
-	}
-
-}
+//package io.labber.kbadm.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//// import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//	@Bean
+//	// @Order(2)
+//	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//		http
+//			.csrf(csrf -> csrf.disable()) // Disable CSRF for stateless APIs
+//			.authorizeHttpRequests(
+//				authorizeRequests -> authorizeRequests
+//					.anyRequest().permitAll())
+//					.sessionManagement(sess -> sess
+//					.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // No sessions
+//				);
+//		return http.build();
+//	}
+//
+//}
