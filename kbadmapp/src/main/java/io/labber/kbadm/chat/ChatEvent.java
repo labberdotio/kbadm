e8be3cd46ed84ad837cb4220e31c09a99f1a2e49
John Grundback <jgrundback@gmail.com>
2026-06-27 17:08:20 +0200

    Implementing a streaming chat service via Spring AI.
    Implementing a streaming chat service via Spring AI.

---
+++ kbadmapp/src/main/java/io/labber/kbadm/chat/ChatEvent.java
@@ -0,0 +1,20 @@
+
+//
+// Copyright (c) 2026, John Grundback
+// All rights reserved.
+//
+
+package io.labber.kbadm.chat;
+
+/**
+ * 
+ * @author john
+ *
+ */
+record ChatEvent(
+	long timestamp, 
+	String type, 
+	String data
+) {
+	// 
+}
