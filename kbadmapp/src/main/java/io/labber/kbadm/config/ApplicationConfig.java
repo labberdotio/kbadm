597d3f4627c1d2476e14ae5e2692a36306a68d15
John Grundback <jgrundback@gmail.com>
2026-06-27 16:50:31 +0200

    Stashing.
    -rw-r--r--@ 1 john  staff  2344 juni 26 15:28 kbadmapp/src/main/java/io/labber/kbadm/config/ApplicationConfig.java

--- kbadmapp/src/main/java/io/labber/kbadm/config/ApplicationConfig.java
+++ kbadmapp/src/main/java/io/labber/kbadm/config/ApplicationConfig.java
@@ -10,6 +10,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EntityScan(basePackages = { "io.labber", "io.labber.app", "io.labber.kbadm" })
@@ -19,6 +22,42 @@ public class ApplicationConfig {
 	@Bean
 	ConfigLoader configLoader() {
 		return new ConfigLoader();
+	}
+
+//	@Bean
+//	public Driver driver() {
+//	    return GraphDatabase.driver("neo4j://<host>:<bolt-port>",
+//	            AuthTokens.basic("<username>", "<password>"));
+//	}
+//
+//	@Bean
+//	public VectorStore vectorStore(Driver driver, EmbeddingModel embeddingModel) {
+//	    return Neo4jVectorStore.builder(driver, embeddingModel)
+//	        .databaseName("neo4j")                // Optional: defaults to "neo4j"
+//	        .distanceType(Neo4jDistanceType.COSINE) // Optional: defaults to COSINE
+//	        .embeddingDimension(1536)                      // Optional: defaults to 1536
+//	        .label("Document")                     // Optional: defaults to "Document"
+//	        .embeddingProperty("embedding")        // Optional: defaults to "embedding"
+//	        .indexName("custom-index")             // Optional: defaults to "spring-ai-document-index"
+//	        .initializeSchema(true)                // Optional: defaults to false
+//	        .batchingStrategy(new TokenCountBatchingStrategy()) // Optional: defaults to TokenCountBatchingStrategy
+//	        .build();
+//	}
+//
+//	// This can be any EmbeddingModel implementation
+//	@Bean
+//	public EmbeddingModel embeddingModel() {
+//	    return new OpenAiEmbeddingModel(new OpenAiApi(System.getenv("OPENAI_API_KEY")));
+//	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
