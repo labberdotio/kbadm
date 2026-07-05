43d7fd187bf4ca49bee20df574f581b66b5ea3c7
John Grundback <jgrundback@gmail.com>
2026-07-05 11:59:33 +0200

    Making the Spring AI streaming chat service compatible with MUI X Chat.
    Making the Spring AI streaming chat service compatible with the MUI X
    Chat ChatBox Adapter.

--- kbadmapp/src/main/java/io/labber/kbadm/chat/ChatTypeResponse.java
+++ kbadmapp/src/main/java/io/labber/kbadm/chat/ChatTypeResponse.java
@@ -13,19 +13,45 @@ package io.labber.kbadm.chat;
  */
 public class ChatTypeResponse {
 
+	private String id;
+	private String messageId;
+
 	private String type;
 	private String content;
 
 	/**
 	 * 
+	 * @param id
+	 * @param messageId
+	 * @param type
 	 * @param content
 	 */
 	public ChatTypeResponse(
+		String id, 
+		String messageId, 
 		String type, 
 		String content
 	) {
+		this.id = id;
+		this.messageId = messageId;
 		this.type = type;
 		this.content = content;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getMessageId() {
+		return messageId;
 	}
 
 	/**
@@ -41,6 +67,14 @@ public class ChatTypeResponse {
 	 * @return
 	 */
 	public String getContent() {
+		return content;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getDelta() {
 		return content;
 	}
 
