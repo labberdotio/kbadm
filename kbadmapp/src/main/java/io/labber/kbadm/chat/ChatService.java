8373df5af7834c285485cdcb1de42804cb1f094a
John Grundback <jgrundback@gmail.com>
2026-07-05 12:13:03 +0200

    Making the Spring AI streaming chat service compatible with MUI X Chat.
    Making the Spring AI streaming chat service compatible with the MUI X
    Chat ChatBox Adapter.

--- kbadmapp/src/main/java/io/labber/kbadm/chat/ChatService.java
+++ kbadmapp/src/main/java/io/labber/kbadm/chat/ChatService.java
@@ -24,6 +24,10 @@ import reactor.core.publisher.Flux;
 @Component
 public class ChatService {
 
+	protected boolean started = false;
+	protected boolean thinking = false;
+	protected boolean texting = false;
+
 	/**
 	 * 
 	 * @param url
@@ -93,37 +97,116 @@ public class ChatService {
 	public Flux<ServerSentEvent<ChatTypeResponse>> flatMap(ChatResponse response) {
 
 		if( response.getResult().getMetadata().containsKey("thinking")) {
+
+			if( this.thinking == false ) {
+				this.thinking = true;
+				return Flux.just(
+					ServerSentEvent.<ChatTypeResponse>builder()
+						.id("message")
+						.event("reasoning-start")
+						.data(
+							new ChatTypeResponse(
+								"message", 
+								"reasoning-1", 
+								"reasoning-start", 
+								// response.getResult().getOutput().getText()
+								response.getResult().getMetadata().get("thinking").toString()
+							)
+						)
+						.build(), 
+					ServerSentEvent.<ChatTypeResponse>builder()
+						.id("message")
+						.event("reasoning-delta")
+						.data(
+							new ChatTypeResponse(
+								"message", 
+								"reasoning-1", 
+								"reasoning-delta", 
+								// response.getResult().getOutput().getText()
+								response.getResult().getMetadata().get("thinking").toString()
+							)
+						)
+						.build()
+				);
+			} else {
+				return Flux.just(
+					ServerSentEvent.<ChatTypeResponse>builder()
+						.id("message")
+						.event("reasoning-delta")
+						.data(
+							new ChatTypeResponse(
+								"message", 
+								"reasoning-1", 
+								"reasoning-delta", 
+								// response.getResult().getOutput().getText()
+								response.getResult().getMetadata().get("thinking").toString()
+							)
+						)
+						.build()
+				);
+			}
+
+		}
+
+		if( this.texting == false ) {
+			this.thinking = false;
+			this.texting = true;
 			return Flux.just(
 				ServerSentEvent.<ChatTypeResponse>builder()
+				.id("message")
+				.event("reasoning-end")
+				.data(
+					new ChatTypeResponse(
+						"message", 
+						"reasoning-1", 
+						"reasoning-end", 
+						// response.getResult().getOutput().getText()
+						// response.getResult().getMetadata().get("thinking").toString()
+						null
+					)
+				)
+				.build(), 
+				ServerSentEvent.<ChatTypeResponse>builder()
 					.id("message")
-					.event("reasoning-delta")
+					.event("text-start")
 					.data(
 						new ChatTypeResponse(
 							"message", 
-							"reasoning-1", 
-							"reasoning-delta", 
-							// response.getResult().getOutput().getText()
-							response.getResult().getMetadata().get("thinking").toString()
+							"text-1", 
+							"text-start", 
+							response.getResult().getOutput().getText()
+						)
+					)
+					.build(), 
+				ServerSentEvent.<ChatTypeResponse>builder()
+					.id("message")
+					.event("text-delta")
+					.data(
+						new ChatTypeResponse(
+							"message", 
+							"text-1", 
+							"text-delta", 
+							response.getResult().getOutput().getText()
+						)
+					)
+					.build()
+			);
+		} else {
+			return Flux.just(
+				ServerSentEvent.<ChatTypeResponse>builder()
+					.id("message")
+					.event("text-delta")
+					.data(
+						new ChatTypeResponse(
+							"message", 
+							"text-1", 
+							"text-delta", 
+							response.getResult().getOutput().getText()
 						)
 					)
 					.build()
 			);
 		}
-
-		return Flux.just(
-			ServerSentEvent.<ChatTypeResponse>builder()
-				.id("message")
-				.event("text-delta")
-				.data(
-					new ChatTypeResponse(
-						"message", 
-						"text-1", 
-						"text-delta", 
-						response.getResult().getOutput().getText()
-					)
-				)
-				.build()
-		);
 
 	}
 
